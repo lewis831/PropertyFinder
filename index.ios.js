b55070b154c655e019441e57ab76ac9e619493db
lewis831 <lewisrashe@Lewiss-MacBook-Air.local>
2017-04-22 21:06:49 -0700

    Added hello world!
    Test

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+//Tutorial used to make this app: https://www.raywenderlich.com/126063/react-native-tutorial
+//For best practices such as error tracking use this https://zaicheng.me/2016/06/20/react-native-initial-setup/
+//Finally don't forget GitHub and Git version control
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+'use strict';
 
-export default class PropertyFinder extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+var React = require('react');
+var ReactNative = require('react-native');
+
+var styles = ReactNative.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
-AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
+class PropertyFinderApp extends React.Component {
+  render() {
+    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello World!");
+  }
+}
