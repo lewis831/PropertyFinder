7ce48064e6302bea1a4c0309db657e629ff2bb31
lewis831 <lewisrashe@Lewiss-MacBook-Air.local>
2017-04-23 02:44:52 -0700

    Used Xcode
    While using Xcode made some changes.

--- Components/SearchPage.js
+++ Components/SearchPage.js
@@ -115,6 +115,23 @@ class SearchPage extends Component {
   _executeQuery(query) {
     console.log(query);
     this.setState({ isLoading: true });
+    fetch(query)
+      .then(response => response.json())
+      .then(json => this._handleResponse(json.response))
+      .catch(error =>
+         this.setState({
+           isLoading: false,
+           message: `Something bad happened ${error}`,
+         }));
+  }
+
+  _handleResponse(response) {
+    this.setState({ isLoading: false, message: '' });
+    if (response.application_response_code.substr(0, 1) === '1') {
+      console.log(`Properties found: ${response.listings.length}`);
+    } else {
+      this.setState({ message: 'Location not recognized; please try again.' });
+    }
   }
 
   render() {
